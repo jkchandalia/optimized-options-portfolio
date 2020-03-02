32ce77a40f95e92ee56841cc81a9d307a1dccd46
Juhi Chandalia <jkchandalia@gmail.com>
2020-03-02 11:02:25 -0800

    More cleanup, get API key from environment

--- oop/option.py
+++ oop/option.py
@@ -1,31 +1,52 @@
 from collections import deque
-import requests
-from . import data
 import datetime
+import os
 import requests
 
+TD_AMERITRADE_API_KEY = os.environ['TD_AMERITRADE_API_KEY']
+ENDPOINT = "https://api.tdameritrade.com/v1/marketdata/chains"
 
-class Option():
-    equity = models.CharField(max_length=255)
-    strike = models.IntegerField()
-    exp_date = models.DateField()
-    option_type = models.CharField(max_length=255)
-    bid = models.FloatField()
-    ask = models.FloatField()
-    mark = models.FloatField()
-    delta = models.FloatField()
-    theta = models.FloatField()
-    gamma = models.FloatField()
-    vega = models.FloatField()
-    equity_price = models.FloatField()
-    flavor = models.CharField(max_length=255)
-    desc = models.CharField(max_length=255)
-    symbol = models.CharField(max_length=255)
-    # daysToExpiration = models.IntegerField(default=None)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = OptionsManager()
+class Option:
+    def __init__(equity, type, strike, exp_date):
+        params={"apikey": TD_AMERITRADE_API_KEY,
+                "symbol": equity,
+                "contractType": contractType.upper(),
+                "strike": str(strike),
+                "fromDate": exp_date,
+                "toDate": exp_date}
+        
+        validation = cls.get_market_data(params)
+        equity = models.CharField(max_length=255)
+        strike = models.IntegerField()
+        exp_date = models.DateField()
+        option_type = models.CharField(max_length=255)
+        bid = models.FloatField()
+        ask = models.FloatField()
+        mark = models.FloatField()
+        delta = models.FloatField()
+        theta = models.FloatField()
+        gamma = models.FloatField()
+        vega = models.FloatField()
+        equity_price = models.FloatField()
+        flavor = models.CharField(max_length=255)
+        desc = models.CharField(max_length=255)
+        symbol = models.CharField(max_length=255)
+        # daysToExpiration = models.IntegerField(default=None)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+        objects = OptionsManager()
+    
+    @classmethod
+    def get_market_data(cls, params):
+        content = requests.get(url = data.endpoint, params = params)
+        api_data=content.json()
+        if api_data['status'] == 'FAILED':
+            return False
+        elif api_data['status'] == 'SUCCESS':
+            output = parse_api_data(api_data, params)
 
+        return output
+    
     def _calculate_margin(self):
         # http://www.cboe.com/LearnCenter/pdf/margin2-00.pdf
         # print(self.equity_price)
@@ -37,7 +58,7 @@ class Option():
         return margin
 
     def _update_w_api_data(self):
-        self.api_data = requests.get(url = data.endpoint, params = self.query_params).json()
+        self.api_data = requests.get(url = ENDPOINT, params = self.query_params).json()
         self._parse_api_data()
         self.save()
 
@@ -81,22 +102,6 @@ class Option():
 
     def update_data(self):
         self._update_w_api_data()
-
-    def validate_option(request):
-        f = request.POST
-        params={"apikey": data.client_id,
-                "symbol": f["equity"],
-                "contractType": f["type"].upper(),
-                "strike": f["strike"],
-                "fromDate": f["exp_date"],
-                "toDate":f["exp_date"]}
-        option_exists = Option.objects.filter(
-            equity=params['symbol'],
-            strike=params['strike'],
-            exp_date=params['fromDate'],
-            option_type=params['contractType']
-        )
-        user=User.objects.get(id=request.session['userid'])
 
         if option_exists:
             option = option_exists[0]
